81ff3e71c844b22b1f2a516839df36004f4c17d3
Owen Hearn <ohearn@f1r4s20.codam.nl>
2022-12-20 18:02:37 +0100

    Cleaning up header file

--- main.c
+++ main.c
@@ -6,14 +6,13 @@
 /*   By: ohearn <ohearn@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 15:33:49 by ohearn        #+#    #+#                 */
-/*   Updated: 2022/12/20 16:27:28 by ohearn        ########   odam.nl         */
+/*   Updated: 2022/12/20 17:07:45 by ohearn        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 
 int	error_handling(t_stack **a, t_stack **b)
 {
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ohearn <ohearn@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 13:34:46 by ohearn        #+#    #+#                 */
-/*   Updated: 2022/12/18 11:54:06 by owen          ########   odam.nl         */
+/*   Updated: 2022/12/20 17:44:57 by ohearn        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,33 +49,40 @@ void		reverse_rotate_a(t_stack **a);
 void		reverse_rotate_b(t_stack **b);
 void		reverse_rotate_both(t_stack **a, t_stack **b);
 
-/*main*/
+/*main and miscellaneous functions*/
 int			error_handling(t_stack **a, t_stack **b);
+void		free_stack(t_stack **stack);
+int			ft_atoi(const char *str);
+
+/*input checking*/
+int			input_check(char **av);
 
 /*position*/
 void		get_target_pos(t_stack **a, t_stack **b);
-int			ft_atoi(const char *str);
-void		ft_putstr(char *str);
-int			input_check(char **av);
-void		free_stack(t_stack **stack);
-int			is_digit(char c);
-int			symbol_check(char c);
-int			comparison_check(const char *s1, const char *s2, int i, int c);
-int			ft_atoi(const char *str);
-t_stack		*new_stack(int value);
-t_stack		*fill_stack(int ac, char **av);
-void		add_end(t_stack **stack, t_stack *new);
-int			stack_size(t_stack *stack);
-t_stack		*search_last(t_stack *stack);
-int			find_top(t_stack *stack);
-int			already_sorted(t_stack *stack);
-void		index_stack(t_stack *stack_a, int stack_size);
-void		sort_three(t_stack **stack);
-void		sort(t_stack **a, t_stack **b);
-void		get_cost(t_stack **a, t_stack **b, int size_a, int size_b);
-int			a_value(int nmb);
-void		optimal_move(t_stack **a, t_stack **b, int cost_a, int cost_b);
-void		find_optimal_move(t_stack **a, t_stack **b);
-int			get_lowest(t_stack **stack);
+
+
+
+
+// void		ft_putstr(char *str);
+// void		free_stack(t_stack **stack);
+// int			is_digit(char c);
+// int			symbol_check(char c);
+// int			comparison_check(const char *s1, const char *s2, int i, int c);
+// int			ft_atoi(const char *str);
+// t_stack		*new_stack(int value);
+// t_stack		*fill_stack(int ac, char **av);
+// void		add_end(t_stack **stack, t_stack *new);
+// int			stack_size(t_stack *stack);
+// t_stack		*search_last(t_stack *stack);
+// int			find_top(t_stack *stack);
+// int			already_sorted(t_stack *stack);
+// void		index_stack(t_stack *stack_a, int stack_size);
+// void		sort_three(t_stack **stack);
+// void		sort(t_stack **a, t_stack **b);
+// void		get_cost(t_stack **a, t_stack **b, int size_a, int size_b);
+// int			a_value(int nmb);
+// void		optimal_move(t_stack **a, t_stack **b, int cost_a, int cost_b);
+// void		find_optimal_move(t_stack **a, t_stack **b);
+// int			get_lowest(t_stack **stack);
 
 #endif
